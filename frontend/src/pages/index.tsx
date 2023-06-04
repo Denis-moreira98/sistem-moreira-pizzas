1931ee7a48389c4c1c88df353eb150cd8c79ea90
Denis Moreira <silvad281@gmail.com>
2023-06-04 16:03:25 -0300

    feat: config incial do front Nextjs

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+   return (
+      <div>
+         <h1>Moreira Pizzas</h1>
+      </div>
+   );
+}
