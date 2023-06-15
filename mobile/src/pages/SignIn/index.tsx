25cc01528234465fcad3dbde35389e11a5f7baa4
Denis Moreira <silvad281@gmail.com>
2023-06-15 12:07:41 -0300

    feat: tela de login

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,10 +1,88 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import {
+   View,
+   Text,
+   StyleSheet,
+   Image,
+   TextInput,
+   TouchableOpacity,
+} from "react-native";
 
 export default function SignIn() {
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+
+   function handleLogin() {
+      if (email === "" || password === "") {
+         return;
+      }
+      alert("email digital: " + email);
+   }
+
    return (
-      <View>
-         <Text>TELA DE LOGIN</Text>
+      <View style={styles.container}>
+         <Image style={styles.logo} source={require("../../assets/logo.png")} />
+
+         <View style={styles.inputContainer}>
+            <TextInput
+               placeholder="Digite seu email"
+               style={styles.input}
+               placeholderTextColor={"#ccc"}
+               value={email}
+               onChangeText={setEmail}
+            />
+            <TextInput
+               placeholder="Digite sua senha"
+               style={styles.input}
+               placeholderTextColor={"#ccc"}
+               value={password}
+               onChangeText={setPassword}
+            />
+            <TouchableOpacity style={styles.button} onPress={handleLogin}>
+               <Text style={styles.buttonText}>Acessar</Text>
+            </TouchableOpacity>
+         </View>
       </View>
    );
 }
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#1D1D2E",
+   },
+   logo: {
+      marginBottom: 18,
+   },
+   input: {
+      width: "95%",
+      height: 40,
+      backgroundColor: "#101026",
+      marginBottom: 12,
+      borderRadius: 4,
+      paddingHorizontal: 8,
+      color: "#fff",
+   },
+   inputContainer: {
+      width: "95%",
+      alignItems: "center",
+      justifyContent: "center",
+      paddingVertical: 32,
+      paddingHorizontal: 14,
+   },
+   button: {
+      width: "95%",
+      height: 40,
+      backgroundColor: "#3fffa3",
+      borderRadius: 4,
+      justifyContent: "center",
+      alignItems: "center",
+   },
+   buttonText: {
+      fontSize: 18,
+      fontWeight: "bold",
+      color: "#102026",
+   },
+});
--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { View } from "react-native/Libraries/Components/View/View";
+import { View, ActivityIndicator } from "react-native";
 
 import AppRoutes from "./app.routes";
 import AuthRoutes from "./Auth.routes";
@@ -8,6 +8,21 @@ import AuthRoutes from "./Auth.routes";
 function Routes() {
    const isAuthenticated = false;
    const loading = false;
+
+   if (loading) {
+      return (
+         <View
+            style={{
+               flex: 1,
+               backgroundColor: "#1D1D2E",
+               justifyContent: "center",
+               alignContent: "center",
+            }}
+         >
+            <ActivityIndicator size={55} color="#FFF" />
+         </View>
+      );
+   }
 
    return isAuthenticated ? <AppRoutes /> : <AuthRoutes />;
 }
