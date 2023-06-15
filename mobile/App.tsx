0e296738d903de5d6df62b0706d96c480a688390
Denis Moreira <silvad281@gmail.com>
2023-06-15 10:40:00 -0300

    feat: navigation

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+
+import SignIn from "./src/pages/SignIn";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   return (
+      <View>
+         <StatusBar
+            backgroundColor="#1d1d2e"
+            barStyle="light-content"
+            translucent={false}
+         />
+         <SignIn />
+      </View>
+   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
---
+++ mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function SignIn() {
+   return (
+      <View>
+         <Text>TELA DE LOGIN</Text>
+      </View>
+   );
+}
