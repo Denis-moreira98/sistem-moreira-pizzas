31030e6e035accbd9c1deeb3caa85144fd11ff91
Denis Moreira <silvad281@gmail.com>
2024-05-05 13:58:06 -0300

    feat: criando componente CardCategory

---
+++ frontend/src/components/CardCategory/index.tsx
@@ -0,0 +1,38 @@
+import { canSSRAuth } from "@/utils/canSSRAuth";
+import styles from "./styles.module.scss";
+import { setupAPIClient } from "@/services/api";
+import { useRouter } from "next/navigation";
+
+interface categoryProps {
+   id: string;
+   name: string;
+}
+
+export function CardCategory({ id, name }: categoryProps) {
+   const router = useRouter();
+
+   async function handleDelete(id) {
+      try {
+         const api = setupAPIClient();
+         await api.delete(`/category?category_id=${id}`);
+         router.refresh();
+      } catch (error) {
+         console.log(error);
+      }
+   }
+   return (
+      <section className={styles.card}>
+         <span>{name}</span>
+         <div className={styles.div_buttons}>
+            <button className={styles.button2} onClick={() => handleDelete(id)}>
+               Excluir
+            </button>
+         </div>
+      </section>
+   );
+}
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+   return {
+      props: {},
+   };
+});
